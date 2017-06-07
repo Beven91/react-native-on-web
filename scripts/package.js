14a2594e1c9e61f40e5dd88b434c78cfb0ae7f46
beven.zhou <beven.zhou@lianshang.com>
2017-06-07 17:09:48 +0800

    修复windows下发布bin 存在\r问题

--- scripts/package.js
+++ scripts/package.js
@@ -12,4 +12,16 @@ var packagefile = path.resolve('cli/tmpl/project/package.json');
 var package = fse.readJSONSync(packagefile);
 package.dependencies['react-native-on-web'] ='^'+selfPackage.version;
 package.devDependencies=selfPackage.devDependencies;
+
+//去除bin下文件的\r
+var binPath  = path.resolve('bin');
+fse.readdirSync(binPath).forEach(handleUnixLF);
+
+//删除bin下文件的\r
+function handleUnixLF(file){
+  var content = new String(fse.readFileSync(path.join(binPath,file)));
+  content = content.replace(/\r/g,'');
+  fse.writeFileSync(file,content);
+}
+
 fse.writeJSONSync(packagefile,package);
