362606c6cdbcb6869401f6ea67c00847583d165d
beven.zhou <beven.zhou@lianshang.com>
2017-06-07 17:49:04 +0800

    添加npm-publish-bin 处理windows下发布bin文件带\r问题

--- scripts/package.js
+++ scripts/package.js
@@ -13,15 +13,4 @@ var package = fse.readJSONSync(packagefile);
 package.dependencies['react-native-on-web'] ='^'+selfPackage.version;
 package.devDependencies=selfPackage.devDependencies;
 
-//去除bin下文件的\r
-var binPath  = path.resolve('bin');
-fse.readdirSync(binPath).forEach(handleUnixLF);
-
-//删除bin下文件的\r
-function handleUnixLF(file){
-  var content = new String(fse.readFileSync(path.join(binPath,file)));
-  content = content.replace(/\r/g,'');
-  fse.writeFileSync(file,content);
-}
-
 fse.writeJSONSync(packagefile,package);
