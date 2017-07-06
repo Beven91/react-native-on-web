3fcebce011e994a23206d9348e29cf8c13d1e4e2
beven.zhou <beven.zhou@lianshang.com>
2017-07-06 18:33:06 +0800

    node端优先加载.web.js

--- cli/packager/register/react-native-register.js
+++ cli/packager/register/react-native-register.js
@@ -4,10 +4,13 @@
  * 描述：用于实现开发环境将具体的模块使用别名模块进行引用
  */
 var path = require('path')
+var fs = require('fs');
 var imageWeb = require('image-web-loader');
 
 // 原始require
 var ORIGINAL_RESOLVE_FILENAME = module.constructor._resolveFilename
+//原始js loader
+var ORIGINAL_JS_EXTENSION = require.extensions['.js'];
 // 配置
 var config = require('../config.js');
 
@@ -17,6 +20,13 @@ var xAssetsPlugin = new (imageWeb.RequireImageXAssetPlugin)(config.imageAssets);
 // 别名模块
 var ReactNativeWebAlias = config.alias || {
   'react-native': 'react-native-on-web'
+}
+
+//重写js加载函数，用于将.web.js优先级提升至js前
+require.extensions['.js'] = function (md, filename) {
+  var webjs = filename.replace(/\.js$/, '.web.js');
+  filename = fs.existsSync(webjs) ? webjs : filename;
+  return ORIGINAL_JS_EXTENSION(md, filename);
 }
 
 /**
