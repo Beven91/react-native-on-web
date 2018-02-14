5fd9eb09e57f08a0356f9ea40f2762f285372d8f
beven <zjayzbwen@163.com>
2018-02-15 01:33:24 +0800

    完善parepare.js脚本

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -2,5 +2,8 @@ var path = require('path');
 var Npm = require('npm-shell');
 
 var npm = new Npm(path.resolve('packages/react-native-on-web-cli/tmpl/project'));
+var rnwNpm = new Npm(path.resolve('packages/react-native-on-web/'));
 
+rnwNpm.install();
+npm.run('link',['../../../react-native-on-web'])
 npm.install();
--- test/index.js
+++ test/index.js
@@ -1,15 +1,10 @@
-var fse = require('fs-extra');
 var path = require('path');
 var Npm = require('npm-shell');
 
-var npm = new Npm(path.resolve('cli/tmpl/project'));
+var cwd = path.resolve('packages/react-native-on-web-cli/tmpl/project');
+var npm = new Npm(cwd);
 
-console.log('copy react-native-on-web')
-//复制cli目录
-fse.copySync(path.resolve('packager'), path.resolve('cli/tmpl/project/node_modules/react-native-on-web/packager'));
-fse.copySync(path.resolve('src'), path.resolve('cli/tmpl/project/node_modules/react-native-on-web/src'));
-fse.copySync(path.resolve('package.json'), path.resolve('cli/tmpl/project/node_modules/react-native-on-web/package.json'));
-
-new Npm(path.resolve('cli/tmpl/project/node_modules/react-native-on-web')).install();
-console.log('start web.......');
-npm.run('start');
+console.log('Start web.......');
+npm.run('start', [], null, {
+  'NODE_PATH': path.join(cwd, 'node_modules')
+});
