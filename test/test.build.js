c3a09b1207652ab12ffbc788a2059ace3682181a
beven <zjayzbwen@163.com>
2018-02-14 19:40:04 +0800

    完善test.build.js

--- test/test.build.js
+++ test/test.build.js
@@ -4,12 +4,10 @@ var Npm = require('npm-shell');
 
 var npm = new Npm(path.resolve('cli/tmpl/project'));
 
-//npm.install();
-
 console.log('copy react-native-on-web')
 //复制cli目录
 fse.copySync(path.resolve('packager'), path.resolve('cli/tmpl/project/node_modules/react-native-on-web/packager'));
-fse.copySync(path.resolve('lib'), path.resolve('cli/tmpl/project/node_modules/react-native-on-web/lib'));
+fse.copySync(path.resolve('src'), path.resolve('cli/tmpl/project/node_modules/react-native-on-web/src'));
 fse.copySync(path.resolve('package.json'), path.resolve('cli/tmpl/project/node_modules/react-native-on-web/package.json'));
 
 new Npm(path.resolve('cli/tmpl/project/node_modules/react-native-on-web')).install();
