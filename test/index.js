0997a871eb0ab774597bfc8efd5ccfa04c4a0589
beven <zjayzbwen@163.com>
2018-04-15 22:24:12 +0800

    完善测试入口

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var Npm = require('npm-shell');
 
-var cwd = path.resolve('packages/react-native-on-web-cli/tmpl/project');
+var cwd = path.resolve('packages/react-native-on-web-tmpl');
 var npm = new Npm(cwd);
 
 console.log('Start web.......');
