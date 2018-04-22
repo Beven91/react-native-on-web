8787c6c28c9524bcb9a9c1b4b049526c86170c71
beven <zjayzbwen@163.com>
2018-04-22 20:11:37 +0800

    删除test/index.js

--- test/index.js
+++
@@ -1,9 +0,0 @@
-var path = require('path');
-var Npm = require('npm-shell');
-
-var cwd = path.resolve('packages/react-native-on-web-tmpl');
-var npm = new Npm(cwd);
-
-npm.run('start', [], null, {
-  'NODE_PATH': path.join(cwd, 'node_modules')
-});
