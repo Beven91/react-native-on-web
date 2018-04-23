3ab9b8434ff759ac1afc056a6459b86949c4c461
zhoubiwen <zhoubiwen@jk.pingan-inc.cn>
2018-04-23 17:05:46 +0800

    5.1.0-beta.13

--- packages/react-native-on-web/packager/rnw-config.js
+++ packages/react-native-on-web/packager/rnw-config.js
@@ -18,6 +18,7 @@ var defaultConfig = {
     'react-native-on-web-index-web-js': path.resolve(web.indexWeb),
     'NativeModules': 'react-native-web',
     'react-native-web/dist/exports/LayoutAnimation': 'react-native-on-web/src/apis/LayoutAnimation/LayoutAnimation',
+    'react-native-web/dist/exports/AsyncStorage': 'react-native-on-web/src/apis/AsyncStorage',
     'react-native-web/dist/exports/Modal': 'react-native-on-web/src/components/Modal/Modal',
     'react-native-web/dist/exports/Navigator': 'react-native-on-web/src/components/Navigator',
     'react-native-web/dist/exports/ToastAndroid': 'react-native-on-web/src/components/ToastAndroid/ToastAndroid',
