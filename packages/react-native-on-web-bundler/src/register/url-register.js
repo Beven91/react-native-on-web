57ea8dfa6fde41cb63c76ee25dd82c17ba6e440e
zhoubiwen <zhoubiwen@jk.pingan-inc.cn>
2018-05-17 11:15:59 +0800

    完善url-register

--- packages/react-native-on-web-bundler/src/register/url-register.js
+++ packages/react-native-on-web-bundler/src/register/url-register.js
@@ -6,13 +6,43 @@
 
 //引入依赖>>
 var fs = require('fs');
+var path = require('path');
+var dantejs = require('dantejs');
 var urlloader = require('url-loader');
+var imageloader = require('image-web-loader');
 
 //全局配置
 var config = require('../rnw-config.js')();
+var webpack = require('../webpack/webpack.client');
 //基础路径
 var publicPath = config.publicPath;
 var serverResolves = config.serverResolves;
+
+var rules = webpack.module.rules || [];
+var imageRule = { test: /\.(png|jpg|jpeg|gif|webp|bmp|ico|jpeg)$/ };
+rules.filter(function (r) {
+  var loaders = dantejs.Array.ensureArray(r.loaders || r.loader);
+  return loaders.filter(function (loader) {
+    var name = (dantejs.Type.isString(loader) ? loader : loader.loader || '');
+    if (name === 'image-web-loader') {
+      imageRule = r;
+    }
+  }).length > 0;
+})
+
+/**
+ * 模块resolve
+ * @param {Module} md 
+ * @param {*} filename 
+ */
+function moduleResolve(md, filename) {
+  const ext = path.extname(filename);
+  if (imageRule.test.test(ext)) {
+    return imgResolve(md, filename);
+  } else {
+    return fileResolver(md, filename);
+  }
+}
 
 /**
  * 扩展require.extensions url-loader 的require实现
@@ -20,34 +50,52 @@ var serverResolves = config.serverResolves;
  * @param filename require的module 对应的文件物理路径
  */
 function fileResolver(md, filename) {
-    var buffer = fs.readFileSync(filename);
-    var context = {
-        resourcePath: filename,
-        query: '?limit=1',
-        options: {},
-        emitFile: function () { }
-    };
-    var exp = urlloader.call(context, buffer);
-    var fn = new Function('module,__webpack_public_path__', exp);
-    fn(md, publicPath);
+  var buffer = fs.readFileSync(filename);
+  var context = getContext(filename, '?limit=1');
+  var exp = urlloader.call(context, buffer);
+  var fn = new Function('module,__webpack_public_path__', exp);
+  fn(md, publicPath);
+}
+
+/**
+ * 图片resolve
+ */
+function imgResolve(md, filename) {
+  var context = getContext(filename,{});
+  var exp = imageloader.sync.call(context,'');
+  var fn = new Function('module,__webpack_public_path__', exp);
+  fn(md, publicPath);
+}
+
+/**
+ * 创建一个loaderContext
+ */
+function getContext(filename, query, options) {
+  return {
+    resourcePath: filename,
+    query: query,
+    options: webpack,
+    emitFile: function () { },
+    emitWarning: function (message) { console.warn(message) }
+  };
 }
 
 /**
  * 空处理模块加载函数
  */
 function unKnowResolve(md, filename) {
-    md.exports = {};
+  md.exports = {};
 }
 
 require.extensions['.css'] = unKnowResolve;
 
 //批量注册静态资源加载器
-config.static.map(function (ext) { (!require.extensions[ext]) && (require.extensions[ext] = fileResolver); });
+config.static.map(function (ext) { (!require.extensions[ext]) && (require.extensions[ext] = moduleResolve); });
 
 
 Object.keys(serverResolves).forEach(function (ext) {
-    var handle = serverResolves[ext];
-    require.extensions['.' + ext] = function (md, file) {
-        md.exports = handle(file);
-    }
+  var handle = serverResolves[ext];
+  require.extensions['.' + ext] = function (md, file) {
+    md.exports = handle(file);
+  }
 })
